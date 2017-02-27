9cdfe78aba49ad019d4b68da80b2ad9fba04af85
luozaichun <952467274@qq.com>
2017-02-27 18:30:16 +0800

    arrange and add pager

--- _src/js/common.js
+++ _src/js/common.js
@@ -3,6 +3,7 @@ require("./jquery-1.8.3.min.js");
 (function () {
     /*导航*/
     var $nav_li= $('#j-nav').find("li");
+    $nav_li.parents(".nav").find(".i-line").css("left",$nav_li.find("a.active").parent().position().left+837+"px");
     $nav_li.hover(function(){
         $(this).parents(".nav").find(".i-line").stop().animate({left: $(this).position().left+837+"px"},200);
     },function () {
--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,39 @@ router.get('/', function(req, res, next) {
 });
 /*分享页*/
 router.get('/share', function(req, res, next) {
-    Miguan_data.fetch(function (error,datas) {
-        if(error){
-            console.log(error);
-        }
-        res.render('share', {
-            title: '分享',
-            content_data:datas
+    var pageSize = req.query.pageSize ? req.query.pageSize :6, curPage = req.query.page ? req.query.page : 1;
+    Miguan_data.find({})
+        .sort({'createAt':-1})
+        .skip((curPage - 1) * pageSize)
+        .limit(pageSize) /*$sort  +  $skip  +  $limit顺序优化*/
+        .exec(function (err,datas) {
+            if(err){
+                console.log(err);
+                return false;
+            }else{
+                Miguan_data.count({},function (err,count) {
+                    if (err){
+                        console.log(err);
+                        return false;
+                    }else{
+                        var totalPages = Math.ceil(count / pageSize);
+                        res.render('share', {
+                                    title: '分享',
+                                    content_data:datas,
+                                    _url: '/share',
+                                    perPage: pageSize,
+                                    curPage: parseInt(curPage),
+                                    totalPages: totalPages,
+                                    active:'share'
+                                });
+                    }
+                })
+            }   
         });
-    });
     
+
+
+
 });
 /*后台编辑路由*/
 router.get('/admin/editor', function (req, res, next) {
@@ -28,15 +51,16 @@ router.post('/admin/add', function (req, res, next) {
     if (req.body.title == '' || req.body.content == '') {
         res.json({code: -1, msg: '参数错误！'});
         return false;
+    }else{
+        Miguan_data.create(req.body, function (err) {
+            if (err) {
+                res.json({code: -1, msg: '数据库错误！'});
+                return false;
+            }
+            console.log(req.body);
+            res.json({code: 1, msg: '添加成功！'})
+        });
     }
-    Miguan_data.create(req.body, function (err) {
-        if (err) {
-            res.json({code: -1, msg: '数据库错误！'});
-            return false;
-        }
-        console.log(req.body);
-        res.json({code: 1, msg: '添加成功！'})
-    });
 });
 /*后台列表*/
 router.get('/admin/list', function (req, res, next) {
